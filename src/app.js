72a853663d242ba0d58582076e4b16f95e22d14b
kuzmavasil74 <98240149+kuzmavasil74@users.noreply.github.com>
2026-07-05 23:38:29 +0200

    Add CORS, record controller logging

--- src/app.js
+++ src/app.js
@@ -1,8 +1,15 @@
 import express from 'express'
 import recordRoutes from './routes/recordRoutes.js'
 import authRoutes from './routes/authRoutes.js'
+import cors from 'cors'
 
 const app = express()
+
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+  })
+)
 app.use(express.json())
 app.use('/auth', authRoutes)
 app.use('/records', recordRoutes)
--- src/controllers/recordController.js
+++ src/controllers/recordController.js
@@ -1,6 +1,7 @@
 import DailyRecord from '../models/DailyRecord.js'
 
 export const createRecord = async (req, res) => {
+  console.log('req.body:', req.body)
   try {
     const {
       siteId,
@@ -22,6 +23,7 @@ export const createRecord = async (req, res) => {
     })
     res.status(201).json({ message: 'Record created successfully', record })
   } catch (error) {
+    console.error('Record error:', error)
     res.status(500).json({ message: 'Error creating record' })
   }
 }
